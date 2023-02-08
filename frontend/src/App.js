057085b165b761bb28813bea91fffb0a051b282f
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2023-02-07 20:45:24 -0800

    updated ticker input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import UserComponent from './components/UserComponent';
 
 function App() {
+  const [currTicker, setTicker] = useState("");
+
+  const handleChange = (event) => {
+    // 👇 Get input value from "event"
+    setTicker(event.target.value);
+  };
+
+  useEffect(() => {
+    console.log("testing input")
+    console.log(currTicker)
+  }, [currTicker]);
+
   return (
     <div className="App">
         <UserComponent />
         <div className="form__group field">
-          <input type="input" className="form__field" placeholder="Name" name="name" id='name' required />
+          <input type="input" className="form__field" onChange={handleChange} placeholder="Name" name="name" id='name' required />
           <label htmlFor="name" className="form__label">Add Ticker</label>
         </div>
     </div>
