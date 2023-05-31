4b2d5b3c74c6e298a052a7a5fd8ecc346426e2ac
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2023-05-31 17:41:12 -0400

    home page looking better

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -6,12 +6,13 @@ export default function Login() {
   
 
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div class="bucket">
+      <h1 class="header">Stock View</h1>
+      <div class = "filler"></div>
       <div class="container">
         <div>
           <input type="text" id="input" class="Input-text" placeholder="Username" />
-        </div>
+         </div>
       </div>
     </div>
   );
