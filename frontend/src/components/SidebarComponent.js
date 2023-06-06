ee716dad3b027eab0bbf61914c625e6d6632df6c
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2023-06-06 15:05:01 -0400

    icons added

--- frontend/src/components/SidebarComponent.js
+++ frontend/src/components/SidebarComponent.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {AiOutlineStock} from 'react-icons/ai'
 
 //Min 1:34:00
 
@@ -7,15 +8,16 @@ const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 bo
 
 const SideBar = () => {
     return ( 
-        <div className='flex flex-col justify-between bg-black h-full overflow-y min-w-210'>
+        <div className='flex flex-col justify-between bg-blue h-full overflow-y min-w-210'>
             <div className='flex flex-col'>
                 <div className='flex flex-col gap-5'>
                     <div className={( {isActive} ) => isActive ? isActiveStyle : isNotActiveStyle}>
-
+                        <AiOutlineStock />
+                        Hello
                     </div>
 
                 </div>
-                Hello
+                
             </div>
 
             
