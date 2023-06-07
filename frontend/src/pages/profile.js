6fc4f8faebf73fcc4148c2d79c3d78b9a55df402
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2023-06-06 23:05:41 -0400

    top left looks good

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -86,7 +86,7 @@ export default function Profile() {
     <div className="App">
         {/* <UserComponent /> */}
 
-        <div>
+        <div className="helloUser">
           Hello {username}
         </div>
 
