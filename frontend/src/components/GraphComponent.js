c5bfa7cd26cc6f4701176449af339867db89f34d
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2023-06-08 21:41:17 -0400

    graph colors look good

--- frontend/src/components/GraphComponent.js
+++ frontend/src/components/GraphComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {XYPlot, XAxis, YAxis, HorizontalGridLines, VerticalGridLines,GradientDefs, LineSeries} from 'react-vis';
+import {XYPlot, XAxis, YAxis, GradientDefs, LineSeries} from 'react-vis';
 import '../../node_modules/react-vis/dist/style.css';
 
 
@@ -7,8 +7,28 @@ import '../../node_modules/react-vis/dist/style.css';
 export default function GraphComponent({
   data
 }){
-  console.log(data)
-  console.log(data[0].y)
+  const start = data[0].y
+
+  const minVal = data.reduce((previous, current) => {
+    return current.y > previous.y ? current : previous;
+  });
+  const maxVal = data.reduce((previous, current) => {
+    return current.y < previous.y ? current : previous;
+  });
+
+  let numerator = start - minVal.y
+  let denominator = maxVal.y - minVal.y
+
+  let percentage = (numerator / denominator) * 100
+
+  let firstQtemp = percentage / 2
+  let thirdQtemp = percentage + (percentage / 2)
+
+  let firstQ = firstQtemp + "%"
+  let thirdQ = thirdQtemp + "%"
+  const mid  = percentage + "%"
+  const zero = "0%"
+
   return (
   <XYPlot width={630} height={380}>
   {/* <HorizontalGridLines style={{stroke: '#B7E9ED'}} />
@@ -16,21 +36,28 @@ export default function GraphComponent({
   <XAxis
     title="X Axis"
     style={{
-      line: {stroke: "#ADDDE1"},
+      line: {stroke: "black"},
       ticks: {stroke: '#ADDDE1'},
       text: {stroke: 'none', fontWeight: 600}
     }}
   />
-  <YAxis title="Y Axis" />
+  <YAxis 
+    title="Y Axis" 
+    color="black"
+  />
   
   <GradientDefs> 
     <linearGradient id="myGradient" gradientTransform="rotate(90)" > 
-      <stop offset="0%" stopColor="#74A04C" /> 
-      <stop offset="25%" stopColor="#74A04C" /> 
-      <stop offset="25%" stopColor="#e5a218" /> 
-      <stop offset="76%" stopColor="#e5a218" /> 
-      <stop offset="76%" stopColor="#CA0016" /> 
-      <stop offset="100%" stopColor="#CA0016" /> 
+      <stop offset={zero} stopColor="#38ef7d" /> 
+      <stop offset={firstQ} stopColor="green" /> 
+      <stop offset={firstQ} stopColor="green" /> 
+      <stop offset={mid} stopColor="black" /> 
+      <stop offset={mid} stopColor="black" /> 
+      <stop offset={thirdQ} stopColor="red" /> 
+      <stop offset={thirdQ} stopColor="red" /> 
+      <stop offset="100%"stopColor="#c21e1e" /> 
+      {/* <stop offset="76%" stopColor="#CA0016" /> 
+      <stop offset="100%" stopColor="#CA0016" />  */}
     </linearGradient> 
   </GradientDefs>
   <LineSeries
